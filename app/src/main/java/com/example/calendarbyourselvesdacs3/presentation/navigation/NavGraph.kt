9020ab1edb9f45240091b9a7ede25470e91dcaea
truongcongly <truongcongly139@gmail.com>
2024-05-19 00:40:00 +0700

    add file for sign in with google

---
+++ app/src/main/java/com/example/calendarbyourselvesdacs3/presentation/navigation/NavGraph.kt
@@ -0,0 +1,108 @@
+package com.example.calendarbyourselvesdacs3.presentation.navigation
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.calendarbyourselvesdacs3.presentation.MainViewModel
+import com.example.calendarbyourselvesdacs3.presentation.home.ProfileScreen
+import com.example.calendarbyourselvesdacs3.presentation.sign_in.GoogleAuthUiClient
+import com.example.calendarbyourselvesdacs3.presentation.sign_in.SignInScreen
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun NavGraph(viewModel: MainViewModel, context: Context) {
+    val navController = rememberNavController()
+    val coroutineScope = rememberCoroutineScope()
+     val googleAuthUiClient by lazy {
+        GoogleAuthUiClient(
+            context = context,
+            oneTapClient = Identity.getSignInClient(context)
+        )
+    }
+
+    NavHost(navController = navController, startDestination = Screen.SignInScreen.name) {
+        composable(Screen.SignInScreen.name) {
+
+            val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+            LaunchedEffect(key1 = Unit) {
+                if(googleAuthUiClient.getSignedInUser() != null) {
+                    navController.navigate(Screen.HomeScreen.name)
+                }
+            }
+
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                onResult = { result ->
+                    if(result.resultCode == Activity.RESULT_OK) {
+                        coroutineScope.launch {
+                            val signInResult = googleAuthUiClient.signInWithIntent(
+                                intent = result.data ?: return@launch
+                            )
+                            viewModel.onSignInResult(signInResult)
+                        }
+                    }
+                }
+            )
+
+            LaunchedEffect(key1 = state.isSignInSuccessfull) {
+                if(state.isSignInSuccessfull) {
+                    Toast.makeText(
+                        context,
+                        "Sign in successful",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    navController.navigate(Screen.HomeScreen.name)
+                    viewModel.resetState()
+                }
+            }
+
+            SignInScreen(
+                state = state,
+                onSignInClick = {
+                    coroutineScope.launch {
+                        val signInIntentSender = googleAuthUiClient.signIn()
+                        launcher.launch(
+                            IntentSenderRequest.Builder(
+                                signInIntentSender ?: return@launch
+                            ).build()
+                        )
+                    }
+                }
+            )
+        }
+        composable(Screen.HomeScreen.name) {
+            ProfileScreen(
+                userData = googleAuthUiClient.getSignedInUser(),
+                onSignOut = {
+                    coroutineScope.launch {
+                        googleAuthUiClient.signOut()
+                        Toast.makeText(
+                            context,
+                            "Signed out",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        navController.popBackStack()
+                    }
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calendarbyourselvesdacs3/presentation/sign_in/data/UserData.kt
@@ -0,0 +1,7 @@
+package com.example.calendarbyourselvesdacs3.presentation.sign_in.data
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilPictureUrl: String?
+)
